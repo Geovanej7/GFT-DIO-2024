805c9e5851e1f1fbfb2e22337ce0bc5d8a1e0447
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2024-08-18 11:42:09 -0300

    first commit

---
+++ src/main/java/me/dio/gtf_dio_2024/GtfDio2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.gtf_dio_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtfDio2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtfDio2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/gtf_dio_2024/GtfDio2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.gtf_dio_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtfDio2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
